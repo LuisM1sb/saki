d0791313296c524d6be26e0ee346d1aff95b9858
LuisM1sb <luismendezmor@gmail.com>
2025-12-27 17:09:50 -0300

    delete horario de soporte

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,12 +47,6 @@ const About = () => {
                     </svg>
                     <span className="text-light-gray">SEO Optimizado</span>
                   </div>
-                  <div className="flex items-center text-soft-gold">
-                    <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20">
-                      <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-                    </svg>
-                    <span className="text-light-gray">Soporte 24/7</span>
-                  </div>
                 </div>
               </div>
 
